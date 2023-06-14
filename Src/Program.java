27537f20f8e6d6e0875aa38b436301c169002f62
VitaliyNovickiy <vidmarev@mail.ru>
2023-06-14 11:39:31 +0300

    FinalCommit

--- Src/Program.java
+++ Src/Program.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Program {
     public static void main(String[] args) {
 
-    }
-}
+           Random random = new Random();
+
+                int num = random.nextInt(100_000) + 11;
+                int temp = num;
+                int sum = 0;
+
+                for (int i = 0; i < 5; i++) {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+
+                System.out.println("Сума чисел числа = " + temp + " = " + sum);
+            }
+        }
+
+
+
+
+
+
